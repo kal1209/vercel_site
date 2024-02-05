1a5f6bed65a500731e018f17a1f5a127dd646442
alex <alexanderho1993@proton.me>
2024-02-05 15:15:42 -0600

    Add project

---
+++ src/Components/SocialLinks.jsx
@@ -0,0 +1,32 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: SocialLinks.jsx
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 03/06/2023
+*/
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icons } from "../Constants/constants";
+
+const SocialLinks = () => {
+  return (
+    <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
+      <a href="https://github.com/kal1209" target="_blank">
+        <FontAwesomeIcon
+          className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
+          icon={icons.faGithub}
+        />
+      </a>
+      <a href="mailto:naruto951021@gmail.com" target="_blank">
+        <FontAwesomeIcon
+          className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
+          icon={icons.faEnvelope}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
